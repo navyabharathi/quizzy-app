1c148da901886832739e57c0df2e90d07d9a553e
Navya <navyabharathi.rameshkumar@anywhere.co>
2025-04-13 23:38:55 +0530

    Delete frontend/.next directory

--- frontend/.next/static/chunks/src_app_quiz_[id]_page_tsx_8450df25._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_a1a4fc45._.js"
-],
-    source: "dynamic"
-});
--- frontend/.next/static/chunks/src_app_quizzes_page_tsx_8450df25._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_8f32173e._.js"
-],
-    source: "dynamic"
-});
